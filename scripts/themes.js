b5368e6a9992d98e8c4ea697b635f681af623e86
Stephen Ritchie <mcplayersjr74@gmail.com>
2024-09-26 13:26:10 -0500

    Update themes.js

--- scripts/themes.js
+++ scripts/themes.js
@@ -4,7 +4,7 @@ function init() {
 function selectChanged() {
   var theme = document.getElementById('theme').value;
   if (theme == 'bo') {
-    document.getElementById("pic").src = "https://stephenritchie74.github.io/themes/blueorange/stephenritchie74.png;"
+    document.getElementById("pic").src = "https://stephenritchie74.github.io/themes/blueorange/stephenritchie74.png";
     document.getElementById("styles").href = "https://stephenritchie74.github.io/themes/blueorange/styles.css";
   } else {
   	document.getElementById("pic").src = "/stephenritchie74.png";
