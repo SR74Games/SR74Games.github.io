cefcae6178b7e13430d6b742c48d05838a738e69
Stephen Ritchie <mcplayersjr74@gmail.com>
2024-09-26 14:10:15 -0500

    Update themes.js

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,14 +1,23 @@
 function init() {
   document.getElementById("theme").onchange = selectChanged;
+  var cookies = document.cookie.split(';');
+  for (var i=0, count=cookies.length; i < count; i++) {
+    if (cookies[i] == "theme=bo") {
+      document.getElementById("pic").src = "https://stephenritchie74.github.io/themes/blueorange/stephenritchie74.png";
+      document.getElementById("styles").href = "https://stephenritchie74.github.io/themes/blueorange/styles.css";
+    }
+  }
 }
 function selectChanged() {
   var theme = document.getElementById('theme').value;
   if (theme == 'bo') {
     document.getElementById("pic").src = "https://stephenritchie74.github.io/themes/blueorange/stephenritchie74.png";
     document.getElementById("styles").href = "https://stephenritchie74.github.io/themes/blueorange/styles.css";
+    document.cookie = "theme=bo";
   } else {
-  	document.getElementById("pic").src = "/stephenritchie74.png";
-    document.getElementById("styles").href = "/styles.css";
+  	document.getElementById("pic").src = "https://stephenritchie74.github.io/stephenritchie74.png";
+    document.getElementById("styles").href = "https://stephenritchie74.github.io/styles.css";
+    document.cookie = "theme=bg";
   }
 }
 window.onload = init;
